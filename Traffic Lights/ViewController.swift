8b5bd36a1838fad8b0442b5ec7964c1deebad120
Nikita Zhuravlev <skill1993@icloud.com>
2021-05-18 23:38:33 +0300

    Added initial UI

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func actionButtonTapped(_ sender: Any) {
+    }
+    
 
 }
 
