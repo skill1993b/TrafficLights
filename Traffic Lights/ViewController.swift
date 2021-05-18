65bd670a8cd9613a40ef5f9e4ee77247a9d76e22
Nikita Zhuravlev <skill1993@icloud.com>
2021-05-19 00:05:25 +0300

    Finished homework task 2

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -15,14 +15,53 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var actionButton: UIButton!
     
+    enum Lights {
+        case red
+        case yellow
+        case green
+    }
     
+    var state = Lights.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setLights()
+        
     }
 
     @IBAction func actionButtonTapped(_ sender: Any) {
+        
+        actionButton.setTitle("NEXT", for: .normal)
+        
+        switch state {
+        case .red:
+            self.state = .yellow
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        case .yellow:
+            self.state = .green
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        case .green:
+            self.state = .red
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        }
+    
+    }
+    
+    func setLights() {
+        redLight.alpha = 0.3
+        redLight.layer.cornerRadius = 52
+        yellowLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 52
+        greenLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 52
     }
     
 
